ofedoren/foreman

commit 827ee5926edb24dacac13f2413c4359865813953

Fixes #30470 - Add DSL docs for smart proxy (#7846)

app/models/smart_proxy.rb
@@ -187,6 +187,17 @@ class SmartProxy < ApplicationRecord
     end
   end
 
+  apipie :class, desc: "A class representing #{model_name.human} object" do
+    name 'Smart Proxy'
+    refs 'SmartProxy'
+    sections only: %w[all additional]
+    prop_group :basic_model_props, ApplicationRecord, meta: { friendly_name: 'Smart Proxy' }
+    property :hostname, String, desc: 'Returns name of the host with proxy'
+    property :httpboot_http_port, Integer, desc: 'Returns proxy port for HTTP boot'
+    property :httpboot_http_port!, Integer, desc: 'Same as httpboot_http_port, but raises Foreman::Exception if no port is set'
+    property :httpboot_https_port, Integer, desc: 'Returns proxy port for HTTPS boot'
+    property :httpboot_https_port!, Integer, desc: 'Same as httpboot_https_port, but raises Foreman::Exception if no port is set'
+  end
   class Jail < ::Safemode::Jail
     allow :id, :name, :hostname, :httpboot_http_port, :httpboot_https_port, :httpboot_http_port!, :httpboot_https_port!
   end

commit fba046146d227a9bbc2c4c7041c3a624063bda42

Refs #36575 - Avoid i18n in smart_proxy apipie desc
The model_name.human method actively translates, which can't be used
during start up and is not desired in this context.

Fixes: 827ee5926edb ("Fixes #30470 - Add DSL docs for smart proxy (#7846)")

app/models/smart_proxy.rb
@@ -196,7 +196,7 @@ class SmartProxy < ApplicationRecord
     end
   end
 
-  apipie :class, desc: "A class representing #{model_name.human} object" do
+  apipie :class, desc: "A class representing #{model_name.singular.humanize(capitalize: false)} object" do
     name 'Smart Proxy'
     refs 'SmartProxy'
     sections only: %w[all additional]