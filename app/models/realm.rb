ofedoren/foreman

commit ef8e5137ca4841445a51af8e72fd3383cb40892c

Fixes #29850 - Add DSL docs for realm

app/models/realm.rb
@@ -34,6 +34,11 @@ class Realm < ApplicationRecord
     end
   }
 
+  apipie :class, desc: "A class representing #{model_name.human} object" do
+    sections only: %w[all additional]
+    prop_group :basic_model_props, ApplicationRecord, meta: { example: 'EXAMPLE.COM' }
+    property :realm_type, String, desc: 'Realm type, e.g. FreeIPA or Active Directory'
+  end
   class Jail < ::Safemode::Jail
     allow :id, :name, :realm_type
   end

commit 7ae5f4b499073546931e005778785dac3d4707ed

Refs #36575 - Avoid i18n in realm apipie desc
The model_name.human method actively translates, which can't be used
during start up and is not desired in this context.

Fixes: ef8e5137ca48 ("Fixes #29850 - Add DSL docs for realm")

app/models/realm.rb
@@ -35,7 +35,7 @@ class Realm < ApplicationRecord
     end
   }
 
-  apipie :class, desc: "A class representing #{model_name.human} object" do
+  apipie :class, desc: "A class representing #{model_name.singular.humanize(capitalize: false)} object" do
     sections only: %w[all additional]
     prop_group :basic_model_props, ApplicationRecord, meta: { example: 'EXAMPLE.COM' }
     property :realm_type, String, desc: 'Realm type, e.g. FreeIPA or Active Directory'