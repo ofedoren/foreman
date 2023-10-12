ofedoren/foreman

commit db422306475a72f6f3149d6a36ffcfc2a7ce0ca0

Fixes #29855 - Add DSL docs for users

app/models/user.rb
@@ -144,6 +144,19 @@ class User < ApplicationRecord
     super.tap { |h| h.key?('user') ? h['user']['name'] = name : h['name'] = name }
   end
 
+  apipie :class, desc: "A class representing #{model_name.human} object" do
+    sections only: %w[all additional]
+    property :id, Integer, desc: 'Numerical ID of the User'
+    property :firstname, String, desc: 'Returns the user first name'
+    property :lastname, String, desc: 'Returns the user last name'
+    property :description, String, desc: 'Returns the user description'
+    property :ssh_keys, array_of: SshKey, desc: 'Returns an array of the user\'s associated SshKey objects'
+    property :ssh_authorized_keys, array_of: String, desc: 'Returns an array of strings representing the user\'s keys with their metadata (key, type and comment)'
+    property :login, String, desc: 'Returns the user login'
+    property :mail, String, desc: 'Returns the user mail'
+    property :last_login_on, ActiveSupport::TimeWithZone, desc: 'Returns the user last login time, in UTC time zone'
+    property :disabled, one_of: [true, false], desc: 'Returns true if the user account is disabled, false otherwise'
+  end
   class Jail < ::Safemode::Jail
     allow :id, :login, :ssh_keys, :ssh_authorized_keys, :description, :firstname, :lastname, :mail, :last_login_on, :disabled
   end

commit e46b99af2ee7c36ebd13d7abe39c7613dfcce0eb

Refs #36575 - Avoid i18n in user aipie desc
The model_name.human method actively translates, which can't be used
during start up and is not desired in this context.

Fixes: db422306475a ("Fixes #29855 - Add DSL docs for users")

app/models/user.rb
@@ -149,7 +149,7 @@ class User < ApplicationRecord
 
   set_crud_hooks :user
 
-  apipie :class, desc: "A class representing #{model_name.human} object" do
+  apipie :class, desc: "A class representing #{model_name.singular.humanize(capitalize: false)} object" do
     sections only: %w[all additional]
     property :id, Integer, desc: 'Numerical ID of the User'
     property :firstname, String, desc: 'Returns the user first name'