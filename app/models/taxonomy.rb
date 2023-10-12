ofedoren/foreman

commit db1d64cc389d47e960082f6de63d2e743d4ff920

Fixes #29973 - Move property docs next to the jail

app/models/taxonomy.rb
@@ -1,11 +1,4 @@
 class Taxonomy < ApplicationRecord
-  apipie :prop_group, name: :taxonomy_props do
-    property :title, String, desc: "Title of the #{@meta[:model_name]}. Comparing to the Name, Title contains also names of all parent #{@meta[:model_name]}s, e.g. #{@meta[:example]}"
-    property :description, String, desc: "Description of the #{@meta[:class_scope]}"
-    property :created_at, String, desc: "The time when the #{@meta[:class_scope]} was created"
-    property :updated_at, String, desc: "The last time when the #{@meta[:class_scope]} was updated"
-  end
-
   validates_lengths_from_database
 
   include Authorizable
@@ -46,6 +39,20 @@ class Taxonomy < ApplicationRecord
     child.instance_eval do
       scoped_search :on => :description, :complete_enabled => :false, :only_explicit => true
       scoped_search :on => :id, :validator => ScopedSearch::Validators::INTEGER
+
+      apipie :class, desc: "A class representing #{model_name.human} object" do
+        sections only: %w[all additional]
+        name_exl, title_exl = class_scope.model_name.human == 'Location' ? ['Europe', 'Europe/Prague'] : ['Red Hat', 'Red Hat/Engineering']
+        prop_group :basic_model_props, ApplicationRecord, meta: { example: name_exl }
+        property :title, String, desc: "Title of the #{class_scope}. Comparing to the Name, Title contains also names of all parent #{class_scope}s, e.g. #{title_exl}"
+        property :description, String, desc: "Description of the #{class_scope}"
+        property :created_at, String, desc: "The time when the #{class_scope} was created"
+        property :updated_at, String, desc: "The last time when the #{class_scope} was updated"
+      end
+      jail_class = Class.new(::Safemode::Jail) do
+        allow :id, :name, :title, :created_at, :updated_at, :description
+      end
+      child.const_set('Jail', jail_class)
     end
     child.send(:include, NestedAncestryCommon::Search)
     super

app/models/domain.rb
@@ -1,12 +1,6 @@
 require "resolv"
 # This models a DNS domain and so represents a site.
 class Domain < ApplicationRecord
-  apipie :class, desc: "A class representing #{model_name.human} object" do
-    sections only: %w[all additional]
-    prop_group :basic_model_props, ApplicationRecord, meta: { example: 'example.com' }
-    property :fullname, String, desc: 'User name for this domain, e.g. "Primary domain for our company"'
-  end
-
   audited
   include Authorizable
   extend FriendlyId
@@ -53,6 +47,11 @@ class Domain < ApplicationRecord
     end
   }
 
+  apipie :class, desc: "A class representing #{model_name.human} object" do
+    sections only: %w[all additional]
+    prop_group :basic_model_props, ApplicationRecord, meta: { example: 'example.com' }
+    property :fullname, String, desc: 'User name for this domain, e.g. "Primary domain for our company"'
+  end
   class Jail < Safemode::Jail
     allow :id, :name, :fullname
   end

app/models/ssh_key.rb
@@ -1,18 +1,4 @@
 class SshKey < ApplicationRecord
-  apipie :class, desc: "A class representing #{model_name.human} object" do
-    sections only: %w[all additional]
-    prop_group :basic_model_props, ApplicationRecord, meta: { friendly_name: 'SSH key' }
-    property :user, User, desc: 'Returns the user object which is linked to the SSH key'
-    property :key, String, desc: 'Returns the SSH public key'
-    property :fingerprint, String, desc: 'Returns the fingerprint of the public key'
-    property :length, Integer, desc: 'Returns the length of the SSH public key'
-    property :ssh_key, String, desc: 'Returns the SSH public key'
-    property :type, String, desc: 'Returns the type of the of the SSH key, e.g. ssh-rsa, ssh-dsa',
-                            example: '@ssh_key.type # => "ssh-rsa"'
-    property :comment, String, desc: 'Returns a key comment. The comment is usually a user identifier',
-                               example: '@ssh-key.comment # => forman@foreman.example.com'
-  end
-
   audited :associated_with => :user
   include Authorizable
   extend FriendlyId
@@ -48,6 +34,20 @@ class SshKey < ApplicationRecord
 
   delegate :login, to: :user, prefix: true
 
+  apipie :class, desc: "A class representing #{model_name.human} object" do
+    sections only: %w[all additional]
+    prop_group :basic_model_props, ApplicationRecord, meta: { friendly_name: 'SSH key' }
+    property :user, User, desc: 'Returns the user object which is linked to the SSH key'
+    property :key, String, desc: 'Returns the SSH public key'
+    property :to_export, String, desc: 'Returns a string representing the key with its metadata (key, type and comment)'
+    property :fingerprint, String, desc: 'Returns the fingerprint of the public key'
+    property :length, Integer, desc: 'Returns the length of the SSH public key'
+    property :ssh_key, String, desc: 'Returns the SSH public key'
+    property :type, String, desc: 'Returns the type of the of the SSH key, e.g. ssh-rsa, ssh-dsa',
+                            example: '@ssh_key.type # => "ssh-rsa"'
+    property :comment, String, desc: 'Returns a key comment. The comment is usually a user identifier',
+                               example: '@ssh-key.comment # => forman@foreman.example.com'
+  end
   class Jail < ::Safemode::Jail
     allow :id, :name, :user, :key, :to_export, :fingerprint, :length, :ssh_key, :type, :comment
   end

app/models/taxonomies/location.rb
@@ -1,10 +1,4 @@
 class Location < Taxonomy
-  apipie :class, desc: "A class representing a Location object" do
-    sections only: %w[all additional]
-    prop_group :basic_model_props, ApplicationRecord, meta: { example: 'Europe' }
-    prop_group :taxonomy_props, Taxonomy, meta: { model_name: 'Location', example: 'Europe/Prague' }
-  end
-
   extend FriendlyId
   friendly_id :title
   include Foreman::ThreadSession::LocationModel
@@ -27,10 +21,6 @@ class Location < Taxonomy
   scope :my_locations, lambda { |user = User.current|
     user.admin? ? all : where(id: user.location_and_child_ids)
   }
-
-  class Jail < ::Safemode::Jail
-    allow :id, :name, :title, :created_at, :updated_at, :description
-  end
 
   def dup
     new = super

app/models/taxonomies/organization.rb
@@ -1,10 +1,4 @@
 class Organization < Taxonomy
-  apipie :class, desc: "A class representing an Organization object" do
-    sections only: %w[all additional]
-    prop_group :basic_model_props, ApplicationRecord, meta: { example: 'Red Hat' }
-    prop_group :taxonomy_props, Taxonomy, meta: { model_name: 'Organization', example: 'Red Hat/Engineering' }
-  end
-
   extend FriendlyId
   friendly_id :title
   include Foreman::ThreadSession::OrganizationModel
@@ -27,10 +21,6 @@ class Organization < Taxonomy
   scope :my_organizations, lambda { |user = User.current|
     user.admin? ? all : where(id: user.organization_and_child_ids)
   }
-
-  class Jail < ::Safemode::Jail
-    allow :id, :name, :title, :created_at, :updated_at, :description
-  end
 
   def dup
     new = super

commit 8d69d04c6cc2b44b4ec4929ab9a91d42438e14ec

Refs #36575 - Avoid i18n in taxonomy apipie
The model_name.human method actively translates, which can't be used
during start up and is not desired in this context.

Fixes: db1d64cc389d ("Fixes #29973 - Move property docs next to the jail")

app/models/taxonomy.rb
@@ -37,9 +37,9 @@ class Taxonomy < ApplicationRecord
       scoped_search :on => :description, :complete_enabled => :false, :only_explicit => true
       scoped_search :on => :id, :validator => ScopedSearch::Validators::INTEGER
 
-      apipie :class, desc: "A class representing #{model_name.human} object" do
+      apipie :class, desc: "A class representing #{model_name.singular.humanize(capitalize: false)} object" do
         sections only: %w[all additional]
-        name_exl, title_exl = class_scope.model_name.human == 'Location' ? ['Europe', 'Europe/Prague'] : ['Red Hat', 'Red Hat/Engineering']
+        name_exl, title_exl = class_scope.model_name.to_s == 'Location' ? ['Europe', 'Europe/Prague'] : ['Red Hat', 'Red Hat/Engineering']
         prop_group :basic_model_props, ApplicationRecord, meta: { example: name_exl }
         property :title, String, desc: "Title of the #{class_scope}. Comparing to the Name, Title contains also names of all parent #{class_scope}s, e.g. #{title_exl}"
         property :description, String, desc: "Description of the #{class_scope}"