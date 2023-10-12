ofedoren/foreman

commit 023255082012e16f44b54b712964dfc7b7541ac3

Fixes #29858 - Add DSL docs for domains

app/models/domain.rb
@@ -1,6 +1,12 @@
 require "resolv"
 # This models a DNS domain and so represents a site.
 class Domain < ApplicationRecord
+  apipie :class, desc: "A class representing #{model_name.human} object" do
+    sections only: %w[all additional]
+    prop_group :basic_model_props, ApplicationRecord, meta: { example: 'example.com' }
+    property :fullname, String, desc: 'User name for this domain, e.g. "Primary domain for our company"'
+  end
+
   audited
   include Authorizable
   extend FriendlyId

commit 6e38d12ffb38576b554d48359afe3f5cd0b9f4ca

Refs #36575 - Avoid i18n in domain apipie desc
The model_name.human method actively translates, which can't be used
during start up and is not desired in this context.

Fixes: 023255082012e16f44b54b712964dfc7b7541ac3 ("Fixes #29858 - Add DSL docs for domains")

app/models/domain.rb
@@ -51,7 +51,7 @@ class Domain < ApplicationRecord
 
   set_crud_hooks :domain
 
-  apipie :class, desc: "A class representing #{model_name.human} object" do
+  apipie :class, desc: "A class representing #{model_name.singular.humanize(capitalize: false)} object" do
     sections only: %w[all additional]
     prop_group :basic_model_props, ApplicationRecord, meta: { example: 'example.com' }
     property :fullname, String, desc: 'User name for this domain, e.g. "Primary domain for our company"'