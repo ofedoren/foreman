ofedoren/foreman

commit 9edadc222b95550a2f2d696b313bd3eaa43ebf89

Fixes #29859 - Add DSL docs for SSH keys

app/models/ssh_key.rb
@@ -1,4 +1,18 @@
 class SshKey < ApplicationRecord
+  apipie :class, desc: "A class representing #{model_name.human} object" do
+    sections only: %w[all additional]
+    prop_group :basic_model_props, ApplicationRecord, meta: { friendly_name: 'SSH key' }
+    property :user, User, desc: 'Returns the user object which is linked to the SSH key'
+    property :key, String, desc: 'Returns the SSH public key'
+    property :fingerprint, String, desc: 'Returns the fingerprint of the public key'
+    property :length, Integer, desc: 'Returns the length of the SSH public key'
+    property :ssh_key, String, desc: 'Returns the SSH public key'
+    property :type, String, desc: 'Returns the type of the of the SSH key, e.g. ssh-rsa, ssh-dsa',
+                            example: '@ssh_key.type # => "ssh-rsa"'
+    property :comment, String, desc: 'Returns a key comment. The comment is usually a user identifier',
+                               example: '@ssh-key.comment # => forman@foreman.example.com'
+  end
+
   audited :associated_with => :user
   include Authorizable
   extend FriendlyId

config/initializers/apipie.rb
@@ -7,6 +7,7 @@ ApipieDSL.configure do |config|
   config.doc_base_url = '/templates_doc'
   config.markup = ApipieDSL::Markup::Markdown.new if Rails.env.development? && defined? Maruku
   config.dsl_classes_matchers = [
+    "#{Rails.root}/app/models/**/*.rb",
     "#{Rails.root}/lib/foreman/renderer/**/*.rb",
   ]
   # TODO provisioning reports jobs

commit 051a385f69a25f453b9e0b2f4446e4c04b8fab38

Refs #36575 - Avoid i18n in ssh_key apipie desc
The model_name.human method actively translates, which can't be used
during start up and is not desired in this context.

Fixes: 9edadc222b95 ("Fixes #29859 - Add DSL docs for SSH keys")

app/models/ssh_key.rb
@@ -35,7 +35,7 @@ class SshKey < ApplicationRecord
 
   delegate :login, to: :user, prefix: true
 
-  apipie :class, desc: "A class representing #{model_name.human} object" do
+  apipie :class, desc: "A class representing #{model_name.singular.humanize(capitalize: false)} object" do
     sections only: %w[all additional]
     prop_group :basic_model_props, ApplicationRecord, meta: { friendly_name: 'SSH key' }
     property :user, 'User', desc: 'Returns the user object which is linked to the SSH key'