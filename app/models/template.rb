ofedoren/foreman

commit 8d6c949b86a6e2d46df4a8637bd5fc5523194ff6

Fixes #29852 - Add DSL docs for PTables

app/models/template.rb
@@ -25,6 +25,10 @@ class Template < ApplicationRecord
 
   attr_exportable :name, :description, :snippet, :template_inputs, :model => ->(template) { template.class.to_s }
 
+  apipie :class, desc: "A class representing #{model_name.human} object" do
+    sections only: %w[all additional]
+    prop_group :basic_model_props, ApplicationRecord
+  end
   class Jail < Safemode::Jail
     allow :id, :name
   end

app/models/ptable.rb
@@ -60,6 +60,14 @@ class Ptable < Template
     end
   }
 
+  apipie :class, desc: 'A class representing Partition Table object' do
+    sections only: %w[all additional]
+    prop_group :basic_model_props, ApplicationRecord, meta: { friendly_name: 'partition table', example: 'Kickstart default' }
+  end
+  class Jail < Safemode::Jail
+    allow :id, :name
+  end
+
   def self.template_includes
     super + [:operatingsystems]
   end

commit 62885c5c80f0088374aad14d76537c2715f4ba80

Refs #36575 - Avoid i18n in template apipie desc
The model_name.human method actively translates, which can't be used
during start up and is not desired in this context.

Fixes: 8d6c949b86a6 ("Fixes #29852 - Add DSL docs for PTables")

app/models/template.rb
@@ -25,7 +25,7 @@ class Template < ApplicationRecord
 
   attr_exportable :name, :description, :snippet, :template_inputs, :model => ->(template) { template.class.to_s }
 
-  apipie :class, desc: "A class representing #{model_name.human} object" do
+  apipie :class, desc: "A class representing #{model_name.singular.humanize(capitalize: false)} object" do
     sections only: %w[all additional]
     prop_group :basic_model_props, ApplicationRecord
   end